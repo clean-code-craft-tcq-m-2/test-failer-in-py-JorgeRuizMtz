c945dad8c3d70499eebbd99654570828671716b5
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2022-11-14 14:49:40 -0500

    Add files via upload

--- misaligned_fixed.py
+++ misaligned_fixed.py
@@ -13,9 +13,12 @@ def print_color_map():
             #print("{:<7}|".format(interval), "{:<7}|".format(major), "{:<7}|".format(minor) )
             #text = f'{interval} | {major} | {minor}'
             #print(format(text, '=^20'))
-            s = 6
-            text = '| {:^6} | {:^6} | {:^6} |'.format(interval, major, minor)
-            print(text)
+            space = 6
+            assert space >= 6, "Format not aligned"
+            #text = '| {:^6} | {:^6} | {:^6} |'.format(interval, major, minor)
+            #text = (f'{interval} | {major} | {minor}')
+            #print(text)
+            print('|',f'{interval}'.center(space),'|',f'{major}'.center(space),'|',f'{minor}'.center(space),'|')
             
          
     #return len(major_colors) * len(minor_colors)
