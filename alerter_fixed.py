96dcb627463fb7a96600ed3bb104a5d4c28c75a8
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2022-11-30 16:47:42 -0600

    Update alerter_fixed.py

--- alerter_fixed.py
+++ alerter_fixed.py
@@ -17,11 +17,6 @@ def alert_in_celcius(farenheit):
     returnCode = network_alert_stub(celcius)
     if returnCode != 200:
         print("An error happend \n")
-        # non-ok response is not an error! Issues happen in life!
-        # let us keep a count of failures to report
-        # However, this code doesn't count failures!
-        # Add a test below to catch this bug. Alter the stub above, if needed.
-        # print(celcius)
         global alert_failure_count
         alert_failure_count += 0
 
