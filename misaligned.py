8901658697d91bfa4731040736f94497ada96538
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-15 14:20:50 -0500

    Add files via upload

--- misaligned.py
+++ misaligned.py
@@ -2,12 +2,30 @@
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    list = []
+
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            interval = i * 5 + j
+            list.append(interval)
+            #assert list[0] == 0
+            #print(f'{interval} | {major} | {minor}')
+            #print("{:<7}|".format(interval), "{:<7}|".format(major), "{:<7}|".format(minor) )
+            #text = f'{interval} | {major} | {minor}'
+            #print(format(text, '=^20'))
+            space = 6
+            assert space >= 6, "Format not aligned"
+            #text = '| {:^6} | {:^6} | {:^6} |'.format(interval, major, minor)
+            #text = (f'{interval} | {major} | {minor}')
+            #print(text)
+            print('|',f'{interval}'.center(space),'|',f'{major}'.center(space),'|',f'{minor}'.center(space),'|')
+            
+         
+    #return len(major_colors) * len(minor_colors)
+    return list
 
-
+    
+    
 result = print_color_map()
-assert(result == 25)
+assert result[0] == 1 , "Index is not started at 1"
 print("All is well (maybe!)\n")
